5d1f8888729adb180cfac88995cbf6fb1da9d7c0
Soham <sohamjani007@gmail.com>
2021-10-09 13:35:09 +0530

    whats wrong with first code

--- basicexcercises/rock_paper_scissors.py
+++ basicexcercises/rock_paper_scissors.py
@@ -31,4 +31,38 @@ def play(u1, u2):
 print(play(user1_answer, user2_answer))
 
 
+# DIDN'T UNDERSTOOD THE BELOW CODE MUCH.
+
+print('''Please pick one:
+            rock
+            scissors
+            paper''')
+
+while True:
+    game_dict = {'rock': 1, 'scissors': 2, 'paper': 3}
+    player_a = str(input("Player a: "))
+    player_b = str(input("Player b: "))
+    a = game_dict.get(player_a)
+    b = game_dict.get(player_b)
+    dif = a - b
+
+    if dif in [-1, 2]:
+        print('player a wins.')
+        if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+            continue
+        else:
+            print('game over.')
+            break
+    elif dif in [-2, 1]:
+        print('player b wins.')
+        if str(input('Do you want to play another game, yes or no?\n')) == 'yes':
+            continue
+        else:
+            print('game over.')
+            break
+    else:
+        print('Draw.Please continue.')
+        print('')
+
+
 
